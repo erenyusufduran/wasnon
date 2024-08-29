624a8ab8efcb4e4044e148d67892019d70c0ae7c
Eren Yusuf Duran <eren.duran@colins.com>
2024-08-29 13:19:03 +0300

    routes package deleted and moved to server.go

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/erenyusufduran/wasnon/internal/handlers"
 	"github.com/erenyusufduran/wasnon/internal/repositories"
-	"github.com/erenyusufduran/wasnon/internal/routes"
 	"github.com/labstack/echo/v4"
 
 	"gorm.io/gorm"
@@ -18,8 +17,34 @@ func InitializeServer(db *gorm.DB, repositories *repositories.Repositories) *ech
 	companyHandler := handlers.NewCompanyHandler(repositories.CompanyRepository)
 	workerHandler := handlers.NewWorkerHandler()
 
-	// Register routes
-	routes.RegisterRoutes(e, productHandler, companyHandler, workerHandler)
+	registerRoutes(e, productHandler, companyHandler, workerHandler)
 
 	return e
+}
+
+// registerRoutes sets up the routes for the application
+func registerRoutes(
+	e *echo.Echo,
+	productHandler *handlers.ProductHandler,
+	companyHandler *handlers.CompanyHandler,
+	workerHandler *handlers.WorkerHandler) {
+	registerCompanyRoutes(e, companyHandler)
+	registerProductRoutes(e, productHandler)
+	registerWorkerRoutes(e, workerHandler)
+}
+
+func registerCompanyRoutes(e *echo.Echo, companyHandler *handlers.CompanyHandler) {
+	e.POST("/companies", companyHandler.CreateCompany)
+	e.GET("/companies", companyHandler.ListCompanies)
+}
+
+func registerProductRoutes(e *echo.Echo, productHandler *handlers.ProductHandler) {
+	e.POST("/products", productHandler.CreateProduct)
+	e.GET("/products", productHandler.ListProducts)
+}
+
+func registerWorkerRoutes(e *echo.Echo, workerHandler *handlers.WorkerHandler) {
+	e.POST("/workers/start/:name", workerHandler.StartWorker)
+	e.POST("/workers/stop/:name", workerHandler.StopWorker)
+	e.GET("/workers/status", workerHandler.CheckStatus)
 }
